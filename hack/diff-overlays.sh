82db5566ffd54d5b6d97dc4bff014bc814091456
fmilichovsky <31818576+fmilichovsky@users.noreply.github.com>
2023-11-19 18:22:15 +0100

    Include profile diffing in CI

--- hack/diff-overlays.sh
+++ hack/diff-overlays.sh
@@ -1,23 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <group> <first-overlay-name> <second-overlay-name>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <overlay-1> <overlay-2>"
     exit 1
 fi
 
-group="$1"
-first_overlay="$2"
-second_overlay="$3"
-
-path_first_overlay="${group}/overlays/${first_overlay}"
-path_second_overlay="${group}/overlays/${second_overlay}"
+first_overlay="$1"
+second_overlay="$2"
 
 temp_file_first=$(mktemp)
 temp_file_second=$(mktemp)
 
-kustomize build "$path_first_overlay" > "$temp_file_first"
-kustomize build "$path_second_overlay" > "$temp_file_second"
+# Fail on any kustomization build failures.
+set -e
+kustomize build "$first_overlay" > "$temp_file_first"
+kustomize build "$second_overlay" > "$temp_file_second"
+set +e
 
-diff -u "$temp_file_first" "$temp_file_second"
+diff --unified "$temp_file_first" "$temp_file_second"
 
 rm -f "$temp_file_first" "$temp_file_second"
